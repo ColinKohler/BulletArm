db65cb34d191ae244662c625e954c4b95951b45d
Dian Wang <wangdian1007@gmail.com>
2019-04-08 13:51:02 -0400

    switch observation xy axis in pybullet to match other envs

--- envs/pybullet_env.py
+++ envs/pybullet_env.py
@@ -118,6 +118,7 @@ class PyBulletEnv(BaseEnv):
     image_arr = pb.getCameraImage(width=self.heightmap_size, height=self.heightmap_size,
                                   viewMatrix=self.view_matrix, projectionMatrix=self.proj_matrix)
     self.heightmap = image_arr[3] - np.min(image_arr[3])
+    self.heightmap = self.heightmap.T
 
     return self.getHoldingState(), self.heightmap.reshape([self.heightmap_size, self.heightmap_size, 1])
 
