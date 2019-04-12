fix rotation judgement in grasping AGAIN; increase minimal cube distance

envs/numpy_env.py
@@ -119,7 +119,7 @@ class NumpyEnv(BaseEnv):
   def _generateShapes(self, object_type, num_objects, min_distance=None, padding=None, random_orientation=False):
     ''''''
     if min_distance is None:
-      min_distance = np.sqrt(2) * self.heightmap_size/7
+      min_distance = 2 * self.heightmap_size/7
     if padding is None:
       padding = self.heightmap_size/5
     self.objects = list()

numpy_toolkit/object_generation.py
@@ -58,8 +58,9 @@ class Cube(object):
     valid_rot3 = valid_rot1 + np.pi
     valid_rot4 = valid_rot2 + np.pi
     valid_rots = np.array([valid_rot1, valid_rot2, valid_rot3, valid_rot4])
+    angle = np.pi - np.abs(np.abs(valid_rots - grasp_rot) - np.pi)
     return np.allclose(grasp_pos[:-1], self.pos[:-1], atol=(self.size/2)) and \
-           np.any(np.abs(valid_rots - grasp_rot) < np.pi/7) and \
+           np.any(angle < np.pi/7) and \
            grasp_pos[-1] < self.pos[-1] and \
            self.on_top
 