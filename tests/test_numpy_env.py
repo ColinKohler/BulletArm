da302cfd9b7a4fd6cd9f7f4614040ef2ef4bb461
Dian Wang <wangdian1007@gmail.com>
2019-04-22 11:47:44 -0400

    revert changes in test

--- tests/test_numpy_env.py
+++ tests/test_numpy_env.py
@@ -4,74 +4,21 @@ import matplotlib.pyplot as plt
 import torch
 
 import helping_hands_rl_envs.env_factory as env_factory
-from helping_hands_rl_envs.envs.numpy_env import NumpyEnv
-from helping_hands_rl_envs.envs.block_stacking_env import createBlockStackingEnv
-from helping_hands_rl_envs.envs.block_picking_env import createBlockPickingEnv
 
 workspace = np.asarray([[0, 100],
                         [0, 100],
                         [0, 500]])
 
-env_config = {'workspace': workspace, 'max_steps': 1, 'obs_size': 100, 'render': True, 'action_sequence': 'pxyrz',
-              'num_objects': 1, 'seed': np.random.randint(100), 'random_orientation': True, 'reward_type': 'sparse'}
-env = createBlockPickingEnv(NumpyEnv, env_config)()
-states, obs = env.reset()
-plt.imshow(obs.squeeze())
-plt.show()
+env_config = {'workspace': workspace, 'max_steps': 10, 'obs_size': 100, 'render': True, 'action_sequence': 'xy',
+              'num_objects': 1}
+envs = env_factory.createEnvs(1, 'numpy', 'block_picking', env_config)
 
-pos = env.getObjectPosition()
-rewards = np.zeros((8, 8))
-for i in range(8):
-  for j in range(8):
-    env.saveState()
-    action = torch.tensor([0, pos[0][0], pos[0][1], i*(np.pi/8), j*(10./8)])
-    (states_, obs_), reward, dones = env.step(action)
-    rewards[i, j] = reward
-    # plt.imshow(obs_.squeeze());
-    # plt.show()
-    env.restoreState()
+states, obs = envs.reset()
+for i in range(1, 3):
+  plt.imshow(obs.squeeze(), cmap='gray')
+  plt.show()
+  actions = torch.tensor([[i * 10, i * 10]])
+  states_, obs_, rewards, dones = envs.step(actions)
 
-plt.imshow(obs.squeeze())
-plt.show()
-action = torch.tensor([0, pos[0][0], pos[0][1]])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-env.saveState()
-
-action = torch.tensor([0, pos[1][0], pos[1][1]])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-env.restoreState()
-
-action = torch.tensor([0, 100, 100])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-action = torch.tensor([0, 150, 150])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-env.saveState()
-
-action = torch.tensor([1, 100, 100])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-env.restoreState()
-
-action = torch.tensor([1, 100, 100])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-action = torch.tensor([0, 100, 100])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-action = torch.tensor([1, 50, 50])
-(states_, obs_), rewards, dones = env.step(action)
-plt.imshow(obs_.squeeze()); plt.show()
-
-obs = obs_
-pass
+  obs = obs_
+  pass
