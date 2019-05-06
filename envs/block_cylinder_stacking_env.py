126bec923a9a161ff65457c84b16f5700b56c433
Dian Wang <wangdian1007@gmail.com>
2019-05-06 16:20:19 -0400

    add dense reward in block cylinder env

--- envs/block_cylinder_stacking_env.py
+++ envs/block_cylinder_stacking_env.py
@@ -24,20 +24,24 @@ def createBlockCylinderStackingEnv(simulator_base_env, config):
       self.random_orientation = config['random_orientation'] if 'random_orientation' in config else False
       self.num_obj = config['num_objects'] if 'num_objects' in config else 1
       self.reward_type = config['reward_type'] if 'reward_type' in config else 'sparse'
-      self.min_top = self.num_obj
       if self.num_obj % 2 != 0:
         raise ValueError('number of objects must be plural')
+
+      self.cylinder_stacked = False
+      self.block_stacked = False
 
     def step(self, action):
       self.takeAction(action)
       self.wait(100)
       obs = self._getObservation()
       done = self._checkTermination()
-      curr_num_top = self._getNumTopBlock()
       if self.reward_type == 'dense':
-        if 0 < curr_num_top < self.min_top:
-          reward = float(self.min_top - curr_num_top)
-          self.min_top = curr_num_top
+        if not self.cylinder_stacked and self._getNumTopCylinder() == 1:
+          self.cylinder_stacked = True
+          reward = 1.0
+        elif not self.block_stacked and self._getNumTopBlock() == 1:
+          self.block_stacked = True
+          reward = 1.0
         else:
           reward = 0.0
       else:
@@ -54,18 +58,21 @@ def createBlockCylinderStackingEnv(simulator_base_env, config):
       super(BlockStackingEnv, self).reset()
       self.blocks = self._generateShapes(0, int(self.num_obj/2), random_orientation=self.random_orientation)
       self.cylinders = self._generateShapes(2, int(self.num_obj/2), random_orientation=self.random_orientation)
-      self.min_top = self.num_obj
+      self.cylinder_stacked = False
+      self.block_stacked = False
       return self._getObservation()
 
     def saveState(self):
       super(BlockStackingEnv, self).saveState()
-      self.stacking_state = {'min_top': deepcopy(self.min_top)}
+      self.stacking_state = {'cylinder_stacked': deepcopy(self.cylinder_stacked),
+                             'block_stacked': deepcopy(self.block_stacked)}
 
     def restoreState(self):
       super(BlockStackingEnv, self).restoreState()
       self.blocks = self._getBlocks()
       self.cylinders = self._getCylinders()
-      self.min_top = self.stacking_state['min_top']
+      self.cylinder_stacked = self.stacking_state['cylinder_stacked']
+      self.block_stacked = self.stacking_state['block_stacked']
 
     def _checkTermination(self):
       ''''''
