0a3dcff720c7588ed70172937ce9321f91bd11ef
Dian Wang <wangdian1007@gmail.com>
2019-09-17 12:26:15 -0400

    test via env runner

--- tests/unittests/test_bullet_house_3.py
+++ tests/unittests/test_bullet_house_3.py
@@ -1,9 +1,11 @@
 import unittest
 import time
 import numpy as np
+import torch
 
 from helping_hands_rl_envs.envs.house_building_3_env import createHouseBuilding3Env
 from helping_hands_rl_envs.envs.pybullet_env import PyBulletEnv
+from helping_hands_rl_envs import env_factory
 
 class TestBulletHouse3(unittest.TestCase):
   workspace = np.asarray([[0.35, 0.65],
@@ -15,12 +17,14 @@ class TestBulletHouse3(unittest.TestCase):
 
 
   def testPlanner(self):
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    # env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
     for i in range(5, -1, -1):
       action = env.getPlan()
-      (states_, obs_), rewards, dones = env.step(np.array(action))
-      self.assertEqual(rewards, i)
+      states_, obs_, rewards, dones = env.step(action)
+      self.assertEqual(rewards.item(), i)
+    env.close()
 
 
   # def testPlanner2(self):
@@ -33,7 +37,7 @@ class TestBulletHouse3(unittest.TestCase):
   #     states, obs = env.reset()
   #     for _ in range(10):
   #       action = env.getPlan()
-  #       (states_, obs_), rewards, dones = env.step(np.array(action))
+  #       states_, obs_, rewards, dones = env.step(action)
   #       if dones:
   #         break
   #     s += int(rewards)
@@ -43,226 +47,237 @@ class TestBulletHouse3(unittest.TestCase):
   def testBlockNotValidTriangleOnBrick(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
-    env.saveState()
-    position = list(env.getObjectPosition())
-    action = [0, position[2][0], position[2][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    env.save()
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[2][0], position[2][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
-    action = [1, position[3][0], position[3][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[3][0], position[3][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 8)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
-    env.restoreState()
+    env.restore()
+    env.close()
 
   def testBlockNotValidBrickOrRoofOnBlock(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
-    env.saveState()
-    position = list(env.getObjectPosition())
-    action = [0, position[3][0], position[3][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    env.save()
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[3][0], position[3][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 8)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
-    env.restoreState()
+    env.restore()
 
-    position = list(env.getObjectPosition())
-    action = [0, position[2][0], position[2][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[2][0], position[2][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 8)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
+
+    env.close()
 
   def testBlockNotValidRoofOnBrickOnBlock(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
-    env.saveState()
-    position = list(env.getObjectPosition())
-    action = [0, position[3][0], position[3][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    env.save()
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[3][0], position[3][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 8)
 
-    position = list(env.getObjectPosition())
-    action = [0, position[2][0], position[2][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[2][0], position[2][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 9)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 10)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 9)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 8)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 7)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
+
+    env.close()
 
   def testBlockValidTriangleOnBrick(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
 
-    position = list(env.getObjectPosition())
-    action = [0, position[2][0], position[2][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[2][0], position[2][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
-    action = [1, position[3][0], position[3][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[3][0], position[3][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
+
+    env.close()
 
   def testBlockValidBrickOrRoofOnBlock(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
 
-    env.saveState()
-    position = list(env.getObjectPosition())
-    action = [0, position[3][0], position[3][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    env.save()
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[3][0], position[3][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 3)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 3)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 2)
-    env.restoreState()
+    env.restore()
 
-    position = list(env.getObjectPosition())
-    action = [0, position[2][0], position[2][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    position = list(env.getObjPositions())[0]
+    action = torch.tensor([0, position[2][0], position[2][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
-    action = [1, position[0][0], position[0][1], np.pi / 2]
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    action = torch.tensor([1, position[0][0], position[0][1], np.pi / 2]).unsqueeze(0)
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 6)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
+
+    env.close()
 
   def testSuccess(self):
     self.env_config['random_orientation'] = False
 
-    env = createHouseBuilding3Env(PyBulletEnv, self.env_config)()
+    env = env_factory.createEnvs(1, 'pybullet', 'house_building_3', self.env_config)
     states, obs = env.reset()
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 5)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 4)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 3)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 2)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 1)
 
     action = env.getPlan()
-    (states_, obs_), rewards, dones = env.step(np.array(action))
+    states_, obs_, rewards, dones = env.step(action)
     self.assertEqual(rewards, 0)
+
+    env.close()
