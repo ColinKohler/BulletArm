09e42656231d156133f0d3f7434de50adab4557e
Dian Wang <wangdian1007@gmail.com>
2020-02-17 14:58:32 -0500

    decrease pre offset to 0.1 to improve ik

--- envs/pybullet_env.py
+++ envs/pybullet_env.py
@@ -82,9 +82,9 @@ class PyBulletEnv(BaseEnv):
     self.min_block_size = self.block_original_size * self.block_scale_range[0]
     self.max_block_size = self.block_original_size * self.block_scale_range[1]
 
-    self.pick_pre_offset = 0.15
+    self.pick_pre_offset = 0.1
     self.pick_offset = 0.005
-    self.place_pre_offset = 0.15
+    self.place_pre_offset = 0.1
     self.place_offset = self.block_scale_range[1]*self.block_original_size
 
     # Setup camera parameters
