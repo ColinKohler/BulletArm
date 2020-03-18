43418d9de1e86fb789ebb8075f2820a3ea850bb2
Dian Wang <wangdian1007@gmail.com>
2020-03-18 12:27:30 -0400

    remove some objects from RANDOM set

--- simulators/pybullet/objects/random_object.py
+++ simulators/pybullet/objects/random_object.py
@@ -19,20 +19,16 @@ class RandomObject(PybulletObject):
     urdf_pattern = os.path.join(root_dir, constants.URDF_PATH, 'random_urdfs/*/*.urdf')
     found_object_directories = glob.glob(urdf_pattern)
     if z_scale == 2:
-      found_object_directories = list(filter(lambda x: re.search(r'(000|001|002|005|006|009|011|014|020|023|025|027|032'
-                                                                 r'|034|038|039|052|057|058|059|066|069|070|074|075|078'
-                                                                 r'|080|081|083|089|090|091|104|110|114|115|116|117|118'
-                                                                 r'|119|122|126|130|131|132|133|137|142|143|145|147|148'
-                                                                 r'|149|154|157|159|161|163|166|174|176|182|183|184|185'
-                                                                 r'|187|190|191|196|199|200)_2\.urdf', x),
+      found_object_directories = list(filter(lambda x: re.search(r'(001|002|005|007|009|011|020|023|027|032|034|039|052'
+                                                                 r'|057|058|059|066|069|070|075|080|081|083|089|090|091'
+                                                                 r'|114|115|116|118|122|131|137|142|143|145|149|154|157'
+                                                                 r'|159|161|163|176|182|183|187|190|191|199|200)_2\.urdf', x),
                                              found_object_directories))
     else:
-      found_object_directories = list(filter(lambda x: re.search(r'(000|001|002|005|006|009|011|014|020|023|025|027|032'
-                                                                 r'|034|038|039|052|057|058|059|066|069|070|074|075|078'
-                                                                 r'|080|081|083|089|090|091|104|110|114|115|116|117|118'
-                                                                 r'|119|122|126|130|131|132|133|137|142|143|145|147|148'
-                                                                 r'|149|154|157|159|161|163|166|174|176|182|183|184|185'
-                                                                 r'|187|190|191|196|199|200)\.urdf', x),
+      found_object_directories = list(filter(lambda x: re.search(r'(001|002|005|007|009|011|020|023|027|032|034|039|052'
+                                                                 r'|057|058|059|066|069|070|075|080|081|083|089|090|091'
+                                                                 r'|114|115|116|118|122|131|137|142|143|145|149|154|157'
+                                                                 r'|159|161|163|176|182|183|187|190|191|199|200)\.urdf', x),
                                              found_object_directories))
     total_num_objects = len(found_object_directories)
     urdf_filepath = found_object_directories[np.random.choice(np.arange(total_num_objects), 1)[0]]
