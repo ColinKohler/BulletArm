997930c089615d089e350b695ab38768c8295a4f
Ondrej Biza <ondrej.biza@gmail.com>
2020-08-17 15:25:07 -0400

    custom labels

--- envs/house_building_x_env.py
+++ envs/house_building_x_env.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from helping_hands_rl_envs.envs.pybullet_env import PyBulletEnv
 from helping_hands_rl_envs.simulators import constants
 from .utils.check_goal import CheckGoal
+from .utils.check_goal_custom_labels import CheckGoalCustomLabels
 
 def createHouseBuildingXEnv(simulator_base_env, config):
   class HouseBuildingXEnv(simulator_base_env):
@@ -18,6 +19,9 @@ def createHouseBuildingXEnv(simulator_base_env, config):
       self.random_orientation = config['random_orientation'] if 'random_orientation' in config else False
       self.num_obj = config['num_objects'] if 'num_objects' in config else 1
       self.reward_type = config['reward_type'] if 'reward_type' in config else 'sparse'
+      self.get_custom_labels = config['get_custom_labels'] if 'get_custom_labels' in config else False
+      self.custom_label_goals = config['custom_label_goals'] if 'custom_label_goals' in config else None
+      self.previous_candidates = None
 
       self.gen_blocks = config["gen_blocks"]
       self.gen_bricks = config["gen_bricks"]
@@ -27,7 +31,19 @@ def createHouseBuildingXEnv(simulator_base_env, config):
       goal = config["goal_string"]
       self.check_goal = CheckGoal(goal, self)
 
+      if self.get_custom_labels:
+        assert self.custom_label_goals is not None
+        # create a list of labelers for each task
+        # labels give information about intermediate steps in addition to goal states
+        self.check_goal_labels = [CheckGoalCustomLabels(g, self) for g in self.custom_label_goals]
+
     def step(self, action):
+
+      if self.get_custom_labels:
+        if self.previous_candidates is None:
+          # we want to compare the env state before and after place action to label it
+          self.previous_candidates = [obj.get_place_candidate_dict() for obj in self.check_goal_labels]
+
       self.takeAction(action)
       self.wait(100)
       obs = self._getObservation(action)
@@ -38,7 +54,26 @@ def createHouseBuildingXEnv(simulator_base_env, config):
         done = self.current_episode_steps >= self.max_steps or not self.isSimValid()
       self.current_episode_steps += 1
 
-      return obs, reward, done
+      if self.get_custom_labels:
+        if obs[0]:
+          # get labels for pick action
+          labels = [obj.get_label_pick() for obj in self.check_goal_labels]
+          self.previous_candidates = [obj.get_place_candidate_dict() for obj in self.check_goal_labels]
+        else:
+          # get labels for place action
+          labels = []
+          candidates = []
+
+          for idx, obj in enumerate(self.check_goal_labels):
+            tmp_labels, tmp_candidates = obj.get_label_place(self.previous_candidates[idx])
+            labels.append(tmp_labels)
+            candidates.append(tmp_candidates)
+
+          self.previous_candidates = candidates
+
+        return obs, reward, done, {"labels": labels}
+      else:
+        return obs, reward, done
 
     def reset(self):
       ''''''
