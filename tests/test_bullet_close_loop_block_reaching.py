78fb2115f9355bf03a7d11fc91bab7deb32292f0
Dian Wang <wangdian1007@gmail.com>
2021-08-15 11:09:33 -0400

    add empty close function in single runner

--- tests/test_bullet_close_loop_block_reaching.py
+++ tests/test_bullet_close_loop_block_reaching.py
@@ -12,7 +12,7 @@ class TestBulletBowlStacking(unittest.TestCase):
                           [-0.2, 0.2],
                           [0.01, 0.25]])
   env_config = {'workspace': workspace, 'max_steps': 100, 'obs_size': 128, 'render': False, 'fast_mode': True,
-                'seed': 0, 'action_sequence': 'pxyz', 'num_objects': 1, 'random_orientation': False,
+                'seed': 0, 'action_sequence': 'pxyzr', 'num_objects': 1, 'random_orientation': True,
                 'reward_type': 'sparse', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'kuka',
                 'workspace_check': 'point', 'physics_mode': 'fast', 'hard_reset_freq': 1000, 'object_scale_range': (1, 1),
                 'obs_type': 'vec'}
@@ -20,9 +20,9 @@ class TestBulletBowlStacking(unittest.TestCase):
   planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi/8}
 
   def testPlanner2(self):
-    self.env_config['render'] = False
+    self.env_config['render'] = True
     self.env_config['seed'] = 0
-    num_processes = 5
+    num_processes = 1
     env = env_factory.createEnvs(num_processes, 'pybullet', 'close_loop_block_reaching', self.env_config, self.planner_config)
     total = 0
     s = 0
