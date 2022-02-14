141052519664b7ff4080280dd69916edbe67d9e7
ColinKohler <colink78@gmail.com>
2022-02-14 16:53:52 -0500

    Added logger bones which includes a logger, plotter, and exporter. The logger contains logs and writes to tensorboard during training. Exporter can export tensorboard log data to a different format. The plotter contains helpers to plot the logged data.

---
+++ baselines/logger/exporter.py
@@ -0,0 +1,12 @@
+import csv
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
+import torch.utils.tensorboard import SummaryWriter
+
+class Exporter(Object):
+  '''
+
+  '''
+  def __init(self):
+    pass
---
+++ baselines/logger/plotter.py
@@ -0,0 +1,47 @@
+import numpy as np
+import more_itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
+class Plotter(object):
+  '''
+  Plotting utility.
+  '''
+  def __init__(self):
+    pass
+
+  def plotLearningCurves(self, eps_rewards, title, legend_titles, filepath, window=100):
+    '''
+    Plot mulitple learning curves on a single plot.
+
+    Args:
+      eps_rewards ():
+      title (str):
+      legend_titles (list[str]):
+      filepath (str):
+      window (int):
+    '''
+    pass
+
+  def plotLearningCurve(self, eps_rewards, title, filepath, window=100):
+    '''
+    Plot the learning curve for the given episode rewards.
+
+    Args:
+      eps_rewards (list[double]): Episode rewards to plot
+      title (str): Figure title
+      filepath (str): Filepath to save the plot to
+      window (int): Window to average episode rewards over. Default: 100
+    '''
+    avg_reward = np.mean(list(more_itertools.windowed(eps_rewards, window)), axis=1)
+    xs = np.arange(window, len(avg_reward) + window)
+
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Avg. Reward')
+
+    ax.plot(xs, avg_reward)
+    plt.savefig(filepath)
+    plt.close()
