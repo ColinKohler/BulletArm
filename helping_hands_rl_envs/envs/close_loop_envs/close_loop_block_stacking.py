acc5f9437be209ff94c609c7ef6afaafa6074ba5
Dian Wang <wangdian1007@gmail.com>
2022-04-11 14:52:18 -0400

    add NoValidPositionException handling in close loop block stacking

--- helping_hands_rl_envs/envs/close_loop_envs/close_loop_block_stacking.py
+++ helping_hands_rl_envs/envs/close_loop_envs/close_loop_block_stacking.py
@@ -5,6 +5,7 @@ from helping_hands_rl_envs.pybullet.utils import constants
 from helping_hands_rl_envs.envs.close_loop_envs.close_loop_env import CloseLoopEnv
 from helping_hands_rl_envs.pybullet.utils import transformations
 from helping_hands_rl_envs.planners.close_loop_block_stacking_planner import CloseLoopBlockStackingPlanner
+from helping_hands_rl_envs.pybullet.utils.constants import NoValidPositionException
 
 class CloseLoopBlockStackingEnv(CloseLoopEnv):
   def __init__(self, config):
@@ -12,9 +13,15 @@ class CloseLoopBlockStackingEnv(CloseLoopEnv):
     assert self.num_obj >= 2
 
   def reset(self):
-    self.resetPybulletWorkspace()
-    self.robot.moveTo([self.workspace[0].mean(), self.workspace[1].mean(), 0.2], transformations.quaternion_from_euler(0, 0, 0))
-    self._generateShapes(constants.CUBE, self.num_obj, random_orientation=self.random_orientation)
+    while True:
+      self.resetPybulletWorkspace()
+      self.robot.moveTo([self.workspace[0].mean(), self.workspace[1].mean(), 0.2], transformations.quaternion_from_euler(0, 0, 0))
+      try:
+        self._generateShapes(constants.CUBE, self.num_obj, random_orientation=self.random_orientation)
+      except NoValidPositionException as e:
+        continue
+      else:
+        break
     return self._getObservation()
 
   def _getValidOrientation(self, random_orientation):
