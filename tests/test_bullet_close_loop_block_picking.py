ad4e3afb1a6333f34135aa1741b2c34fae901aab
Dian Wang <wangdian1007@gmail.com>
2022-05-11 11:02:57 -0400

    update test parameters

--- tests/test_bullet_close_loop_block_picking.py
+++ tests/test_bullet_close_loop_block_picking.py
@@ -7,17 +7,10 @@ import matplotlib.pyplot as plt
 
 from helping_hands_rl_envs import env_factory
 
-class TestBulletBowlStacking(unittest.TestCase):
-  workspace = np.asarray([[0.3, 0.6],
-                          [-0.15, 0.15],
-                          [0.01, 0.25]])
-  env_config = {'workspace': workspace, 'max_steps': 50, 'obs_size': 128, 'render': False, 'fast_mode': True,
-                'seed': 0, 'action_sequence': 'pxyzr', 'num_objects': 1, 'random_orientation': True,
-                'reward_type': 'sparse', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'panda',
-                'workspace_check': 'point', 'physics_mode': 'fast', 'hard_reset_freq': 1000, 'object_scale_range': (1, 1),
-                'view_type': 'render_center'}
+class TestBulletCloseLoopBlockPicking(unittest.TestCase):
+  env_config = {}
 
-  planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi/8}
+  planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi / 4}
 
   def testPlanner2(self):
     self.env_config['render'] = True
--- tests/test_bullet_close_loop_block_pulling.py
+++ tests/test_bullet_close_loop_block_pulling.py
@@ -7,17 +7,11 @@ import matplotlib.pyplot as plt
 
 from helping_hands_rl_envs import env_factory
 
-class TestBulletBowlStacking(unittest.TestCase):
-  workspace = np.asarray([[0.25, 0.65],
-                          [-0.2, 0.2],
-                          [0.01, 0.25]])
-  env_config = {'workspace': workspace, 'max_steps': 100, 'obs_size': 128, 'render': False, 'fast_mode': True,
-                'seed': 0, 'action_sequence': 'pxyzr', 'num_objects': 1, 'random_orientation': True,
-                'reward_type': 'sparse', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'kuka',
-                'workspace_check': 'point', 'physics_mode': 'fast', 'hard_reset_freq': 1000, 'object_scale_range': (1, 1),
-                'view_type': 'render_center_height', 'obs_type': 'pixel'}
+class TestBulletCloseLoopBlockPulling(unittest.TestCase):
+  env_config = {}
 
-  planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi/8}
+  planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi / 4}
+
 
   def testPlanner2(self):
     self.env_config['render'] = True
