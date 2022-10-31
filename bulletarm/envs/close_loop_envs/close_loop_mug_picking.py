81fe44438eaa44b517f81ce4e7e00a3e8505864e
ColinKohler <colink78@gmail.com>
2022-10-31 12:02:14 -0400

    Fixing goal checking on mug grasping.

--- bulletarm/envs/close_loop_envs/close_loop_mug_picking.py
+++ bulletarm/envs/close_loop_envs/close_loop_mug_picking.py
@@ -38,5 +38,5 @@ class CloseLoopMugPickingEnv(CloseLoopEnv):
     mug_grasp_pos = self.objects[-1].getGraspPosition()
 
     return (self.robot.holding_obj == self.objects[-1] and
-            np.allclose(gripper_pos, mug_grasp_pos, atol=1e-2) and
+            np.allclose(gripper_pos[:2], mug_grasp_pos[:2], atol=1e-2) and
             gripper_pos[-1] > 0.15)
