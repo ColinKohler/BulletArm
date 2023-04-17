d36125c1414054dc0ec2bfc4e68f284ca34d204e
ColinKohler <colink78@gmail.com>
2023-04-17 12:57:03 -0400

    peg insertion planner update

--- bulletarm/envs/close_loop_envs/close_loop_peg_insertion.py
+++ bulletarm/envs/close_loop_envs/close_loop_peg_insertion.py
@@ -35,7 +35,7 @@ class CloseLoopPegInsertionEnv(CloseLoopEnv):
       constants.SQUARE_PEG,
       pos=[[self.workspace[0].mean(), self.workspace[1].mean(), 0.31]],
       rot=[[0,0,0,1]],
-      scale=0.114, wait=False
+      scale=0.108, wait=False
     )[0]
 
     self.robot.gripper.close()
@@ -50,12 +50,12 @@ class CloseLoopPegInsertionEnv(CloseLoopEnv):
     hole_pos, hole_rot = self.peg_hole.getHolePose()
     peg_pos = self.peg.getPosition()
 
-    return np.allclose(hole_pos[:2], peg_pos[:2], atol=1e-2) and peg_pos[2] < 0.11
+    return np.allclose(hole_pos[:2], peg_pos[:2], atol=1e-2) and peg_pos[2] < 0.12
 
   def _getReward(self):
     hole_pos, hole_rot = self.peg_hole.getHolePose()
     peg_pos = self.peg.getPosition()
-    success_reward = 1 if np.allclose(hole_pos[:2], peg_pos[:2], atol=1e-2) and peg_pos[2] < 0.11 else 0
+    success_reward = 1 if np.allclose(hole_pos[:2], peg_pos[:2], atol=1e-2) and peg_pos[2] < 0.12 else 0
 
     return success_reward
 
